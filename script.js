cf22684a1688293bac7f967166a5605f0594d312
Julien-DRANE <183376865+Julien-DRANE@users.noreply.github.com>
2024-11-26 19:46:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const feedUrls = [
     "https://www.elpais.com/rss/feed.html?feed=elpais/internacional",
     "https://www.globo.com/rss/",
     "https://www.sbs.com.au/news/feed",
-    "https://www.dailymaverick.co.za/feeds/",
+    "https://www.dailymaverick.co.za",
     "https://www.hindustantimes.com/rss/world-news/rssfeed.xml"
 ];
 
